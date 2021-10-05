91e38f255acd4aca50c650ad2500c7316a021710
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-05 16:15:23 +0200

    Added file index.pho

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+//// Definire una classe ‘Movie’
+//// Dichiarare delle variabili d'istanza (proprietà)
+//// Definire un costruttore
+//// Definire almeno un metodo
+//// Istanziare almeno due oggetti ‘Movie’ e stampare a schermo i valori delle relative proprietà (se possibile senza var_dump)
