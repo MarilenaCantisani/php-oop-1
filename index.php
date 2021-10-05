00d5d68b49f36d8adb563105e26f20acfd711025
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-05 17:17:23 +0200

    Printed on page all istances

--- index.php
+++ index.php
@@ -1,10 +1,69 @@
 <?php
 
-//// Definire una classe ‘Movie’
-//// Dichiarare delle variabili d'istanza (proprietà)
-//// Definire un costruttore
-//// Definire almeno un metodo
-//// Istanziare almeno due oggetti ‘Movie’ e stampare a schermo i valori delle relative proprietà (se possibile senza var_dump)
-
 //// Bring back the file containing the movie class
 require_once __DIR__ . '/models/Movie.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+
+<body>
+    <!-- Movie n.1 -->
+    <section id="movie-1">
+        <div class="description">
+            <h1><?= $movie_1->title ?></h1>
+            <address><strong>Directed by:</strong> <?= $movie_1->director ?></address>
+            <span><strong>Genres:</strong> <?= $movie_1->genres ?>; </span>
+            <p><?= $movie_1->plot ?></p>
+        </div>
+        <figure>
+            <img src="<?= $movie_1->cover ?>" alt="<?= $movie_1->title ?>">
+        </figure>
+    </section>
+    <!-- Movie n.2 -->
+    <section id="movie-2">
+        <div class="description">
+            <h1><?= $movie_2->title ?></h2>
+                <address><strong>Directed by:</strong> <?= $movie_2->director ?></address>
+                <span><strong>Genres:</strong> <?= $movie_2->genres ?>; </span>
+                <p><?= $movie_2->plot ?></p>
+        </div>
+        <figure>
+            <img src="<?= $movie_2->cover ?>" alt="<?= $movie_2->title ?>">
+        </figure>
+    </section>
+    <!-- Movie n.3 -->
+    <section id="movie-3">
+        <div class="description">
+            <h1><?= $movie_3->title ?></h3>
+                <address><strong>Directed by:</strong> <?= $movie_3->director ?></address>
+                <span><strong>Genres:</strong> <?= $movie_3->genres ?>; </span>
+                <p><?= $movie_3->plot ?></p>
+        </div>
+        <figure>
+            <img src="<?= $movie_3->cover ?>" alt="<?= $movie_3->title ?>">
+        </figure>
+    </section>
+    <!-- Movie n.4 -->
+    <section id="movie-4">
+        <div class="description">
+            <h1><?= $movie_4->title ?></h4>
+                <address><strong>Directed by:</strong> <?= $movie_4->director ?></address>
+                <span><strong>Genres:</strong> <?= $movie_4->genres ?>; </span>
+                <p><?= $movie_4->plot ?></p>
+        </div>
+        <figure>
+            <img src="<?= $movie_4->cover ?>" alt="<?= $movie_4->title ?>">
+        </figure>
+    </section>
+
+
+</body>
+
+</html>
--- models/Movie.php
+++ models/Movie.php
@@ -44,25 +44,21 @@ $movie_1 = new Movie(1, 'Snowpiercer', 'Action, science-fiction, drama', 'Bong J
 $plot_1 = 'Durante una nuova era glaciale causata da un esperimento fallito, un gruppo di sopravvissuti viaggia su di un treno speciale che non si ferma mai. Le differenze fra i passeggeri, però, portano presto alla rivolta.';
 
 $movie_1->getPlot($plot_1);
-var_dump($movie_1);
 
 //// Instance n.2
 $movie_2 = new Movie(2, 'Inception', 'Action, science-fiction, thriller, adventure', 'Christopher Nolan', 'https://m.media-amazon.com/images/I/51bRJ3i-O8L._SY445_.jpg');
 $plot_2 = 'Dom Cobb possiede una qualifica speciale: è in grado di inserirsi nei sogni altrui per prelevare i segreti nascosti nel più profondo del subconscio. Viene contattato da Saito, un potentissimo industriale giapponese.';
 
 $movie_2->getPlot($plot_2);
-var_dump($movie_2);
 
 //// Instance n.3
 $movie_3 = new Movie(3, 'In Time', 'Action, science-fiction, thriller', 'Andrew Niccol', 'https://pad.mymovies.it/filmclub/2011/07/182/locandina.jpg');
 $plot_3 = 'In un futuro non troppo lontano, il gene dell\'invecchiamento è stato isolato e sconfitto. Per evitare la sovrappopolazione, il tempo è diventato la moneta con cui la gente paga per acquistare i beni.';
 
 $movie_3->getPlot($plot_3);
-var_dump($movie_3);
 
 //// Instance n.4
 $movie_4 = new Movie(4, 'After Earth', 'Science-fiction, action, adventure', 'M. Night Shyamalan', 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/af6166a0df3623baf2cad076c337132221aa8935056e33d2310b8dde003616c3._RI_V_TTW_.jpg');
 $plot_4 = 'Un ranger e suo figlio, provenienti dal pianeta Nova Prime, sono in viaggio con la loro navicella quando vengono costretti ad un atterraggio d\'emergenza sulla Terra, diventata un luogo selvaggio dominato da strane creature.';
 
 $movie_4->getPlot($plot_4);
-var_dump($movie_4);
