f73fbcfb0f135085d40b74a8255ed918c0217bb9
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-05 17:19:38 +0200

    Added css style file

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ require_once __DIR__ . '/models/Movie.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link to css style file -->
+    <link rel="stylesheet" href="css/style.css">
     <title>Movies</title>
 </head>
 
