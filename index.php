ed5e508095c9581170738df1b358fc2c750a9930
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-05 16:26:08 +0200

    Defined a 'movie' class and the related request variables

--- index.php
+++ index.php
@@ -5,3 +5,6 @@
 //// Definire un costruttore
 //// Definire almeno un metodo
 //// Istanziare almeno due oggetti ‘Movie’ e stampare a schermo i valori delle relative proprietà (se possibile senza var_dump)
+
+//// Bring back the file containing the movie class
+require_once __DIR__ . '/models/Movie.php';
---
+++ models/Movie.php
@@ -0,0 +1,11 @@
+<?php
+//// Define a 'movie' class and the related request variables
+
+class Movie
+{
+    public $id;
+    public $title;
+    public $genre;
+    public $director;
+    public $description;
+}
