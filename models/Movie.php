e2838961a472882400ef0ce2b58758dd8f7b9e03
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-05 16:33:29 +0200

    Defined a method that returns the plot of the film

--- models/Movie.php
+++ models/Movie.php
@@ -10,12 +10,16 @@ class Movie
     public $plot;
 
     //* Define a constructor
-    public function __construct($_id, $_title, $_genre, $_director, $_plot)
+    public function __construct($_id, $_title, $_genre, $_director)
     {
         $this->id = $_id;
         $this->title = $_title;
         $this->genre = $_genre;
         $this->director = $_director;
-        $this->plot = $_plot;
+    }
+    //* Define a method
+    public function getPlot()
+    {
+        return $this->plot;
     }
 }
